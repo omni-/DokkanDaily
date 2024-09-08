fee524b7e789e68504ddcc694a28c5587af52a59
Cooper <cooper@stevetex.com>
2024-09-07 21:53:31 -0700

    refactor

---
+++ src/DokkanDaily/Helpers/RNGHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace DokkanDaily.Helpers
+{
+    public static class RNGHelper
+    {
+        public static Random GetDailySeededRandom()
+        {
+            var date = DateTime.UtcNow.Date;
+            var seed = date.Year * 1000 + date.DayOfYear;
+            return new Random(seed);
+        }
+    }
+}
