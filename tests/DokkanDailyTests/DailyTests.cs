d26a2b31d2f3e2e25d2ee58e838a645786454a0b
Cooper <cooper@stevetex.com>
2024-09-23 01:26:33 -0700

    new

--- tests/DokkanDailyTests/DailyTests.cs
+++ tests/DokkanDailyTests/DailyTests.cs
@@ -1,10 +1,11 @@
-using Azure.Storage.Blobs;
 using DokkanDaily.Constants;
 using DokkanDaily.Helpers;
 using DokkanDaily.Models;
+using DokkanDaily.Models.Database;
 using DokkanDaily.Repository;
 using DokkanDaily.Services;
 using DokkanDailyTests.Infra;
+using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
 
@@ -38,19 +39,59 @@ namespace DokkanDailyTests
         }
 
         [Test]
-        public void DailyResetServiceWorks()
+        public async Task DailyResetServiceWorks()
         {
-            // TODO
+            var abMock = mocks.Create<IAzureBlobService>();
+            var repoMock = mocks.Create<IDokkanDailyRepository>();
+            var loggerMock = mocks.Create<ILogger<ResetService>>();
 
-            //var abMock = mocks.Create<IAzureBlobService>();
-            //var repoMock = mocks.Create<IDokkanDailyRepository>();
-            //var loggerMock = mocks.Create<ILogger<TestDailyResetService>>();
+            IResetService tdrs = new ResetService(abMock.Object, repoMock.Object, loggerMock.Object);
 
-            //TestDailyResetService tdrs = new(abMock.Object, repoMock.Object, loggerMock.Object);
+            List<DbClear> actual = [];
 
-            //abMock.Setup(x => x.GetFilesForTag(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(
-            //    [
-            //    ]);
+            abMock
+                .Setup(x => x.GetFilesForTag(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync([
+                    new MockBlobClient(new Dictionary<string, string>()
+                    {
+                        { DDConstants.USER_NAME_TAG, "omni" },
+                        { DDConstants.CLEAR_TIME_TAG, "0'20\"10.8" },
+                        { DDConstants.ITEMLESS_TAG, "true" }
+                    }),
+                    new MockBlobClient(new Dictionary<string, string>()
+                    {
+                        { DDConstants.USER_NAME_TAG, "owl" },
+                        { DDConstants.CLEAR_TIME_TAG, "0'44\"10.8" },
+                        { DDConstants.ITEMLESS_TAG, "false" }
+                    }),
+                    new MockBlobClient(new Dictionary<string, string>()
+                    {
+                        { DDConstants.USER_NAME_TAG, "owl" },
+                        { DDConstants.CLEAR_TIME_TAG, "0'18\"10.8" },
+                        { DDConstants.ITEMLESS_TAG, "false" }
+                    }),
+                    new MockBlobClient(new Dictionary<string, string>()
+                    {
+                        { DDConstants.USER_NAME_TAG, "rabs" },
+                        { DDConstants.CLEAR_TIME_TAG, "0'30\"10.8" },
+                        { DDConstants.ITEMLESS_TAG, "true" }
+                    }),
+                ]);
+
+            repoMock
+                .Setup(x => x.InsertDailyClears(It.IsAny<List<DbClear>>()))
+                .Callback<IEnumerable<DbClear>>(x => actual = x.ToList());
+
+            await tdrs.DoReset();
+
+            List<DbClear> exp = 
+            [
+                new() { DokkanNickname = "omni", ClearTime = "0'20\"10.8", IsDailyHighscore = false, ItemlessClear = true, ClearTimeSpan = new TimeSpan(0, 0, 20, 10, 800) },
+                new() { DokkanNickname = "owl", ClearTime = "0'18\"10.8", IsDailyHighscore = true, ItemlessClear = false, ClearTimeSpan = new TimeSpan(0, 0, 18, 10, 800) },
+                new() { DokkanNickname = "rabs", ClearTime = "0'30\"10.8", IsDailyHighscore = false, ItemlessClear = true, ClearTimeSpan = new TimeSpan(0, 0, 30, 10, 800) }
+            ];
+            
+            actual.Should().BeEquivalentTo(exp);
         }
     }
 }
---
+++ tests/DokkanDailyTests/Infra/MockBlobClient.cs
@@ -0,0 +1,65 @@
+﻿using Azure;
+using Azure.Core;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DokkanDailyTests.Infra
+{
+    internal class MockBlobClient : BlobClient
+    {
+        public Dictionary<string, string> Tags { get; set; } = [];
+
+        private string _name = "";
+        public override string Name => _name;
+
+        public MockBlobClient() { }
+
+        public MockBlobClient(Dictionary<string, string> tags) { Tags = tags; }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        internal class MockResponse : Response
+        {
+            public override int Status => throw new NotImplementedException();
+
+            public override string ReasonPhrase => throw new NotImplementedException();
+
+            public override Stream? ContentStream { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+            public override string ClientRequestId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public override void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override bool ContainsHeader(string name)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override IEnumerable<HttpHeader> EnumerateHeaders()
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override bool TryGetHeader(string name, [NotNullWhen(true)] out string? value)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override bool TryGetHeaderValues(string name, [NotNullWhen(true)] out IEnumerable<string>? values)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override Task<Response<BlobProperties>> GetPropertiesAsync(BlobRequestConditions? conditions = null, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(Response.FromValue<BlobProperties>(BlobsModelFactory.BlobProperties(metadata: Tags), new MockResponse()));
+        }
+    }
+}
