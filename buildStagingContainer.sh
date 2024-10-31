6f615c8f7875aa3cf5ef183d45aafefb341a1932
Cooper <cooper@stevetex.com>
2024-10-31 16:53:05 -0700

    turn on JP parsing on staging

--- buildStagingContainer.sh
+++ buildStagingContainer.sh
@@ -1,3 +1,4 @@
+docker stop dokkandaily.azurecr.io/dokkandaily:staging || true && docker rm dokkandaily.azurecr.io/dokkandaily:staging || true
 docker build . -t dokkandaily.azurecr.io/dokkandaily:staging --build-arg AzureAccountName="%DOTNET_DokkanDailySettings__AzureAccountName%" \ 
 --build-arg AzureConnectionString="%DOTNET_DokkanDailySettings__AzureBlobConnectionString%" \ 
 --build-arg BlobContainerName="stg-daily-clears" \ 
@@ -6,3 +7,4 @@ docker build . -t dokkandaily.azurecr.io/dokkandaily:staging --build-arg AzureAc
 --build-arg OAuth2ClientId="%DOTNET_DokkanDailySettings__OAuth2ClientId%" \ 
 --build-arg SqlServerConnectionString="%DOTNET_DokkanDailySettings__SqlServerConnectionString%" \ 
 --build-arg WebhookUrl="%DOTNET_DokkanDailySettings__WebhookUrl%" \ 
+--build-arg EnableJpParsing="True" \ 
