a1451e4ef49367a454e88accfe62615146f5e0cc
Cooper <cooper@stevetex.com>
2025-01-04 00:20:40 -0800

    Fix loadscript

--- src/DokkanDailyDB/Core/Tables/Daily.sql
+++ src/DokkanDailyDB/Core/Tables/Daily.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [Core].[Daily]
 (
-    [DailyId] INT NOT NULL IDENTITY(1, 1),
+    [DailyId] INT NOT NULL,
     [DailyTypeName] VARCHAR(20) NOT NULL,
 
     CONSTRAINT [DailyPK] PRIMARY KEY CLUSTERED([DailyId] ASC)
--- src/DokkanDailyDB/RefData/LoadDaily.sql
+++ src/DokkanDailyDB/RefData/LoadDaily.sql
@@ -2,13 +2,32 @@
 AS
 BEGIN
 
-    DELETE FROM [Core].[Daily];
+    DECLARE @Source TABLE (
+        DailyId INT NOT NULL, 
+        DailyTypeName VARCHAR(20) NOT NULL
+    )
 
-    INSERT INTO [Core].[Daily]([DailyTypeName])
+    INSERT INTO @Source
     VALUES(
-      'Character'), (
-      'Category'), (
-      'LinkSkill')
+      1, 'Character'), (
+      2, 'Category'), (
+      3, 'LinkSkill')
 
-    RETURN 0;
+    MERGE INTO [Core].[Daily] AS TARGET
+    USING @Source AS SOURCE
+    ON TARGET.DailyId = SOURCE.DailyId
+    WHEN MATCHED AND (
+        TARGET.DailyTypeName COLLATE SQL_Latin1_General_CP1_CS_AS <> SOURCE.DailyTypeName COLLATE SQL_Latin1_General_CP1_CS_AS) THEN
+      UPDATE SET
+        DailyTypeName = TARGET.DailyTypeName
+    WHEN NOT MATCHED BY TARGET THEN
+      INSERT (
+        DailyId,
+        DailyTypeName)
+      VALUES (
+        DailyId,
+        DailyTypeName)
+    WHEN NOT MATCHED BY SOURCE THEN 
+      DELETE;
+
 END
