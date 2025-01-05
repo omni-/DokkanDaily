24984d86bcb451b08c039d075b547f23faf5be77
Cooper <cooper@stevetex.com>
2025-01-04 17:52:43 -0800

    Cleanup

---
+++ src/DokkanDaily/Ocr/ClearScreenUI.cs
@@ -0,0 +1,41 @@
+﻿using SixLabors.ImageSharp;
+
+namespace DokkanDaily.Ocr
+{
+    public class ClearScreenUI(int width, int height)
+    {
+        private static readonly Dictionary<string, RegionLoader.RelativeRegion> Regions = RegionLoader.LoadUIRegions();
+        public Rectangle GetNicknameRegion()
+        {
+            RegionLoader.RelativeRegion normalizedNicknameRegion = Regions["nickname"];
+            return new Rectangle(
+                (int)(normalizedNicknameRegion.X * width),
+                (int)(normalizedNicknameRegion.Y * height),
+                (int)(normalizedNicknameRegion.Width * width),
+                (int)(normalizedNicknameRegion.Height * height)
+            );
+        }
+
+        public Rectangle GetCleartimeRegion()
+        {
+            RegionLoader.RelativeRegion normalizedCleartimeRegion = Regions["cleartime"];
+            return new Rectangle(
+                (int)(normalizedCleartimeRegion.X * width),
+                (int)(normalizedCleartimeRegion.Y * height),
+                (int)(normalizedCleartimeRegion.Width * width),
+                (int)(normalizedCleartimeRegion.Height * height)
+            );
+        }
+
+        public Rectangle GetItemlessRegion()
+        {
+            RegionLoader.RelativeRegion normalizedItemlessRegion = Regions["itemless"];
+            return new Rectangle(
+                (int)(normalizedItemlessRegion.X * width),
+                (int)(normalizedItemlessRegion.Y * height),
+                (int)(normalizedItemlessRegion.Width * width),
+                (int)(normalizedItemlessRegion.Height * height)
+            );
+        }
+    }
+}
--- src/DokkanDaily/Ocr/RegionLoader.cs
+++ src/DokkanDaily/Ocr/RegionLoader.cs
@@ -3,43 +3,6 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace DokkanDaily.Ocr;
 
-public class ClearScreenUI(int width, int height)
-{
-    private static readonly Dictionary<string, RegionLoader.RelativeRegion> Regions = RegionLoader.LoadUIRegions();
-    public Rectangle GetNicknameRegion()
-    {
-        RegionLoader.RelativeRegion normalizedNicknameRegion = Regions["nickname"];
-        return new Rectangle(
-            (int)(normalizedNicknameRegion.X * width),
-            (int)(normalizedNicknameRegion.Y * height),
-            (int)(normalizedNicknameRegion.Width * width),
-            (int)(normalizedNicknameRegion.Height * height)
-        );
-    }
-
-    public Rectangle GetCleartimeRegion()
-    {
-        RegionLoader.RelativeRegion normalizedCleartimeRegion = Regions["cleartime"];
-        return new Rectangle(
-            (int)(normalizedCleartimeRegion.X * width),
-            (int)(normalizedCleartimeRegion.Y * height),
-            (int)(normalizedCleartimeRegion.Width * width),
-            (int)(normalizedCleartimeRegion.Height * height)
-        );
-    }
-
-    public Rectangle GetItemlessRegion()
-    {
-        RegionLoader.RelativeRegion normalizedItemlessRegion = Regions["itemless"];
-        return new Rectangle(
-            (int)(normalizedItemlessRegion.X * width),
-            (int)(normalizedItemlessRegion.Y * height),
-            (int)(normalizedItemlessRegion.Width * width),
-            (int)(normalizedItemlessRegion.Height * height)
-        );
-    }
-}
-
 internal static class RegionLoader
 {
     public record RelativeRegion(float X, float Y, float Width, float Height);
