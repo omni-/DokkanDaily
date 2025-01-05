eaad0edfc06132a9cd44daa5c59de040aba42928
Cooper <cooper@stevetex.com>
2025-01-05 04:28:19 -0800

    Rename storedproc folder

--- scripts/buildDatabase.cmd
+++ scripts/buildDatabase.cmd
@@ -1,4 +1,4 @@
-cd src/DokkanDailyDB
+cd ../src/DokkanDailyDB
 docker image rm -f mydatabase:1.0 || cd .
 docker build . --build-arg PASSWORD="<YourStrong@Passw0rd>" -t mydatabase:1.0 --no-cache
 docker run -p 1433:1433 --name sqldb -d mydatabase:1.0
