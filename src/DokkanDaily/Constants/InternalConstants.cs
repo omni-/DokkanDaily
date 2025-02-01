5fee266e0206142b70a870d669cfe41c82ebf5fd
Cooper <cooper@stevetex.com>
2025-02-01 03:09:31 -0800

    Refactoring

--- src/DokkanDaily/Constants/InternalConstants.cs
+++ src/DokkanDaily/Constants/InternalConstants.cs
@@ -1,8 +1,23 @@
-﻿namespace DokkanDaily.Constants
+﻿using System.Text.Json;
+
+namespace DokkanDaily.Constants
 {
     public static class InternalConstants
     {
         public static string DokkandleDbcRole => "<@&1289820573949497345>";
+
+        public static readonly Dictionary<string, string> KnownUsernameMap = new()
+        {
+            // pattern, value
+            { "五.悟", "五条悟" },
+            { "UBCeomnt", "DBC*omni" }
+        };
+
+        public static readonly JsonSerializerOptions DefaultSerializeOptions = new() { PropertyNameCaseInsensitive = true };
+
+        public static IReadOnlyList<string> Administrators => _administrators.AsReadOnly();
+
+        private static readonly List<string> _administrators = ["112089455933792256", "263499818234675200"];
 
         public static DateTime Season1StartDate => new(2025, 1, 1);
     }
