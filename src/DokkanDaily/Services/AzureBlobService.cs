3f1cd26fdf39522ee0d9b57418330290a7db6cc5
Cooper <cooper@stevetex.com>
2025-02-01 20:26:41 -0800

    null checking

--- src/DokkanDaily/Services/AzureBlobService.cs
+++ src/DokkanDaily/Services/AzureBlobService.cs
@@ -230,8 +230,8 @@ namespace DokkanDaily.Services
             {
                 { AzureConstants.DAILY_TYPE_TAG, model.DailyType.ToString()},
                 { AzureConstants.EVENT_TAG, model.TodaysEvent.FullName},
-                { AzureConstants.USER_NAME_TAG, metadata?.Nickname.EscapeUnicode()},
-                { AzureConstants.ITEMLESS_TAG, metadata?.ItemlessClear.ToString()},
+                { AzureConstants.USER_NAME_TAG, metadata?.Nickname?.EscapeUnicode()},
+                { AzureConstants.ITEMLESS_TAG, (metadata?.ItemlessClear)?.ToString()},
                 { AzureConstants.CLEAR_TIME_TAG, metadata?.ClearTime},
                 { AzureConstants.DISCORD_NAME_TAG, discordUsername },
                 { AzureConstants.DISCORD_ID,  discordId },
