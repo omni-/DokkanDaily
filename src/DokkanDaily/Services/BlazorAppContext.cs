81312a275942f9e31cce0b411e429418a55b06be
Cooper <cooper@stevetex.com>
2025-08-29 11:52:08 -0700

    Only display valid clears

---
+++ src/DokkanDaily/Services/BlazorAppContext.cs
@@ -0,0 +1,10 @@
+﻿namespace DokkanDaily.Services
+{
+    public class BlazorAppContext
+    {
+        /// <summary>
+        /// The IP for the current session
+        /// </summary>
+        public string CurrentUserIP { get; set; }
+    }
+}
