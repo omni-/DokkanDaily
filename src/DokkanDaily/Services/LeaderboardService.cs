df8d345ca04433d0ae81888aa614f101648e307c
Cooper <cooper@stevetex.com>
2026-09-01 21:09:37 -0700

    fix race condition

--- src/DokkanDaily/Services/LeaderboardService.cs
+++ src/DokkanDaily/Services/LeaderboardService.cs
@@ -1,4 +1,4 @@
-﻿using DokkanDaily.Constants;
+using DokkanDaily.Constants;
 using DokkanDaily.Helpers;
 using DokkanDaily.Models;
 using DokkanDaily.Repository;
@@ -8,7 +8,9 @@ namespace DokkanDaily.Services
 {
     public class LeaderboardService(IDokkanDailyRepository repository) : ILeaderboardService
     {
-        private readonly Dictionary<int, List<LeaderboardUser>> _leaderboards = [];
+        private readonly object _lock = new();
+        private readonly Dictionary<int, List<LeaderboardUser>> _leaderboards = new();
+        private readonly Dictionary<int, Task<List<LeaderboardUser>>> _inFlight = new();
         private readonly IDokkanDailyRepository _repository = repository;
         private readonly DateTime _season1Start = InternalConstants.Season1StartDate;
 
@@ -21,34 +23,62 @@ namespace DokkanDaily.Services
 
         public async Task<List<LeaderboardUser>> GetLeaderboardBySeason(int season, bool force = false)
         {
-            if (force || !_leaderboards.TryGetValue(season, out var leaderboard) || leaderboard.Count == 0)
+            if (!force)
             {
-                var result = season == 0 ?
-                    await _repository.GetHallOfFame()
-                    : await _repository.GetLeaderboardByDate(_season1Start.AddMonths(season - 1));
-
-                leaderboard = [];
-
-                foreach (var user in result)
+                Task<List<LeaderboardUser>> fetchTask;
+                lock (_lock)
                 {
-                    leaderboard.Add(new()
+                    if (_leaderboards.TryGetValue(season, out var cached) && cached.Count > 0)
                     {
-                        DiscordUsername = user.DiscordUsername,
-                        DokkanNickname = user.DokkanNickname.UnescapeUnicode(),
-                        DiscordId = user.DiscordId,
-                        TotalHighscores = user.DailyHighscores,
-                        ItemlessClears = user.ItemlessClears,
-                        TotalScore = user.TotalClears + user.ItemlessClears + user.DailyHighscores
-                    });
+                        return cached;
+                    }
+
+                    if (!_inFlight.TryGetValue(season, out fetchTask))
+                    {
+                        fetchTask = FetchAndCache(season);
+                        _inFlight[season] = fetchTask;
+                    }
                 }
 
-                leaderboard = [.. leaderboard
-                    .OrderByDescending(x => x.TotalScore)
-                    .ThenByDescending(x => x.TotalHighscores)
-                    .ThenByDescending(x => x.ItemlessClears)];
+                return await fetchTask;
             }
 
-            _leaderboards[season] = leaderboard;
+            // Force path: bypass cache and deduplication
+            var result = await FetchAndCache(season);
+            return result;
+        }
+
+        private async Task<List<LeaderboardUser>> FetchAndCache(int season)
+        {
+            var dbResult = season == 0
+                ? await _repository.GetHallOfFame()
+                : await _repository.GetLeaderboardByDate(_season1Start.AddMonths(season - 1));
+
+            var leaderboard = new List<LeaderboardUser>();
+
+            foreach (var user in dbResult)
+            {
+                leaderboard.Add(new()
+                {
+                    DiscordUsername = user.DiscordUsername,
+                    DokkanNickname = user.DokkanNickname.UnescapeUnicode(),
+                    DiscordId = user.DiscordId,
+                    TotalHighscores = user.DailyHighscores,
+                    ItemlessClears = user.ItemlessClears,
+                    TotalScore = user.TotalClears + user.ItemlessClears + user.DailyHighscores
+                });
+            }
+
+            leaderboard = [.. leaderboard
+                .OrderByDescending(x => x.TotalScore)
+                .ThenByDescending(x => x.TotalHighscores)
+                .ThenByDescending(x => x.ItemlessClears)];
+
+            lock (_lock)
+            {
+                _leaderboards[season] = leaderboard;
+                _inFlight.Remove(season, out _);
+            }
 
             return leaderboard;
         }
