5cd4e1127a5248c64c42dfcc76f59f3b40a5f932
Cooper <cooper@stevetex.com>
2026-09-11 01:20:13 -0700

    dont need that

--- src/DokkanDailyDB/Post-Deployment/Scripts/PostDeployment.sql
+++ src/DokkanDailyDB/Post-Deployment/Scripts/PostDeployment.sql
@@ -11,8 +11,3 @@ Post-Deployment Script Template
 */
 EXEC RefData.LoadAllData
 GO
--- The schema's permanent unique constraint now owns date uniqueness.
-IF EXISTS (SELECT 1 FROM sys.indexes WHERE object_id = OBJECT_ID('Core.DailyChallenge')
-           AND name = 'DailyChallenge_MigrationDateGuard')
-    DROP INDEX DailyChallenge_MigrationDateGuard ON Core.DailyChallenge;
-GO
